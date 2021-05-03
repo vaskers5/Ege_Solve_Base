bf9820e2a9c8abec676bc09fa706aadbafa3adb4
vaskers5 <danil.krokodil@mail.ru>
2021-05-04 01:07:19 +0300

    Add some solves

---
+++ Data/25/cmake-build-debug/CMakeFiles/25.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "25.exe"
+  "25.exe.manifest"
+  "25.pdb"
+  "CMakeFiles/25.dir/main.cpp.obj"
+  "lib25.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
