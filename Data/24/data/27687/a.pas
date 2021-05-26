78ede4ec1eb2546ca050d926b169389a7063517c
vaskers5 <danil.krokodil@mail.ru>
2021-05-26 16:07:37 +0300

    upd 12 stings

---
+++ Data/24/data/27687/a.pas
@@ -0,0 +1,17 @@
+﻿var maxLen, curLen, i: integer;      
+    s: string;
+begin
+  assign(input, '24_demo.txt');  
+  readln(s);
+  maxLen := 1;
+  curLen := 1;
+  for i:=2 to Length(s) do 
+    if (s[i] = s[i-1]) and (s[i] = 'Y') then begin
+      curLen := curLen + 1;
+      if curLen > maxLen then
+        maxLen := curLen;
+    end
+    else
+      curLen := 1;
+  writeln(maxLen);
+end.
